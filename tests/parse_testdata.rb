8593ce65c0f13be87b600bb69e0312e15fa76c17
Stephen Wardill <s5n.wardill@gmail.com>
2014-03-27 16:10:22 +0000

    added parse_test_data.rb in tests

--- tests/parse_testdata.rb
+++ tests/parse_testdata.rb
@@ -1,16 +1,15 @@
 #encoding: utf-8
 
-require './farn_page_parser.rb'
-fout = File.open('example.txt','w')
-processed = File.open('processing.txt','w')
+require '../farn_page_parser.rb'
+out_dir = 'results'
+warn "Writing any output files to directory #{out_dir}"
 fp = FarnPageParser.new
 
 filenames = Dir.glob("testdata/aaa/*.ht*")
 filenames.sort.each do |f|
-  processed.write("#{f}\n")
+  warn "*** Parsing #{f} ***"
+  fout = File.open("#{out_dir}/#{File.basename(f)}.txt", 'w')
   fp.parse_file(f)
-  fout.puts "//****** HERE #{f} ******//\n"
   fout.puts fp.to_s
+  fout.close
 end
-processed.close
-fout.close
